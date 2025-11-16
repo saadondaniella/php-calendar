e87e17f0a76fdb9947f359af4c9b38b18b4d994d
Daniella Saadon <saadondaniella@gmail.com>
2025-11-16 20:04:23 +0100

    Add Swedish month header to calendar

--- functions.php
+++ functions.php
@@ -2,30 +2,48 @@
 
 function buildCalendar($year, $month, $bookedDays)
 {
+    // Svenska månadsnamn
+    $swedishMonths = [
+        1 => "Januari",
+        2 => "Februari",
+        3 => "Mars",
+        4 => "April",
+        5 => "Maj",
+        6 => "Juni",
+        7 => "Juli",
+        8 => "Augusti",
+        9 => "September",
+        10 => "Oktober",
+        11 => "November",
+        12 => "December"
+    ];
+
+    $monthName = $swedishMonths[$month];
+
     // Antal dagar i månaden
     $daysInMonth = date("t", strtotime("$year-$month-01"));
 
-    // Vilken veckodag första dagen ligger på (1 = måndag, 7 = söndag)
+    // Vilken veckodag den 1:a ligger på
     $firstWeekday = date("N", strtotime("$year-$month-01"));
 
-    $html = '<section class="calendar">';
+    // Rubrik + start på kalendern
+    $html = "<h2 class='month-title'>$monthName $year</h2>";
+    $html .= '<section class="calendar">';
 
-    // Lägg till tomma rutor om månaden inte börjar på måndag
+    // Tomma rutor innan första dagen
     for ($i = 1; $i < $firstWeekday; $i++) {
         $html .= '<div class="day empty"></div>';
     }
 
-    // Riktiga dagar
+    // Dagar i månaden
     for ($day = 1; $day <= $daysInMonth; $day++) {
 
         $classes = ['day'];
 
-        // Bokad
         if (in_array($day, $bookedDays)) {
             $classes[] = 'booked';
         }
 
-        // Helg
         $weekday = date('N', strtotime("$year-$month-$day"));
         if ($weekday == 6 || $weekday == 7) {
             $classes[] = 'weekend';
@@ -36,6 +54,7 @@ function buildCalendar($year, $month, $bookedDays)
         $html .= "<div class=\"$classString\">$day</div>";
     }
 
-    $html .= '</section>';
+    $html .= ' </section>';
+
     return $html;
 }
